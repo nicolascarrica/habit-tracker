0ada5b7bc7d424dd3e3dbcabbd1b7f67863baa1e
Nicolas Carrica <nicolas.carrica@gmail.com>
2026-02-11 08:43:12 -0300

    feat: setup Prisma with Neon database and API test route

---
+++ app/api/test/route.ts
@@ -0,0 +1,12 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const user = await prisma.user.create({
+    data: {
+      id: "nico",
+    },
+  });
+
+  return NextResponse.json(user);
+}
